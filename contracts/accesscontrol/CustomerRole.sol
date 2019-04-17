fa646219ab83105c108ee57df8def0e411e421a8
hujens <jens.hunhevicz@hispeed.ch>
2019-04-17 15:06:01 +0200

    fix: change naming - owner to customer

---
+++ contracts/accesscontrol/CustomerRole.sol
@@ -0,0 +1,48 @@
+pragma solidity ^0.4.24;
+
+// Import the library 'Roles'
+import "./Roles.sol";
+
+// Define a contract 'CustomerRole' to manage this role - add, remove, check
+contract CustomerRole {
+
+  // Define 2 events, one for Adding, and other for Removing
+
+  // Define a struct 'customers' by inheriting from 'Roles' library, struct Role
+
+  // In the constructor make the address that deploys this contract the 1st customer
+  constructor() public {
+    
+  }
+
+  // Define a modifier that checks to see if msg.sender has the appropriate role
+  modifier onlyCustomer() {
+    
+    _;
+  }
+
+  // Define a function 'isCustomer' to check this role
+  function isCustomer(address account) public view returns (bool) {
+    
+  }
+
+  // Define a function 'addCustomer' that adds this role
+  function addCustomer(address account) public onlyCustomer {
+    
+  }
+
+  // Define a function 'renounceCustomer' to renounce this role
+  function renounceCustomer() public {
+    
+  }
+
+  // Define an internal function '_addCustomer' to add this role, called by 'addCustomer'
+  function _addCustomer(address account) internal {
+    
+  }
+
+  // Define an internal function '_removeCustomer' to remove this role, called by 'removeCustomer'
+  function _removeCustomer(address account) internal {
+    
+  }
+}
--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,12 +1,12 @@
 // migrating the appropriate contracts
 var SupplierRole = artifacts.require("./SupplierRole.sol");
 var ContractorRole = artifacts.require("./ContractorRole.sol");
-var OwnerRole = artifacts.require("./OwnerRole.sol");
+var CustomerRole = artifacts.require("./CustomerRole.sol");
 var SupplyChain = artifacts.require("./SupplyChain.sol");
 
 module.exports = function(deployer) {
   deployer.deploy(SupplierRole);
   deployer.deploy(ContractorRole);
-  deployer.deploy(OwnerRole);
+  deployer.deploy(CustomerRole);
   deployer.deploy(SupplyChain);
 };
